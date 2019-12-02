4218c33ba5a9c72dc567ed1f976ee06bf45f89d5
davitoledo <46909172+davitoledo@users.noreply.github.com>
2019-12-02 13:55:31 -0300

    Update and rename index.html to index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<html>
+ <head>
+  <title>526 - Curso Exin DevOps Master</title>
+ </head>
+ <body>
+ <?php 
+echo "<p>Aloha !</p>";
+echo "<p> Exemplo de deploy contínuo com ferramentas SaaS </p>";
+ ?>
+ </body>
+</html>
