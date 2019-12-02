a62f1c4897bf04be60ffbd4593c0c9be601632a1
Davi Toledo <davi_toledo0@hotmail.com>
2019-12-02 15:19:33 -0200

    Inserindo imagem

--- index.php
+++ index.php
@@ -7,5 +7,8 @@
 echo "<p>Olá Mundo! Somos Devopeiros ! </p>";
 echo "<p> Exemplo de deploy contínuo com ferramentas SaaS </p>";
  ?>
+
+<img src="https://www.vanharen.net//media/catalog/product/cache/97/image/9df78eab33525d08d6e5fb8d27136e95/e/x/exin_devops_master.jpg" />
+
  </body>
 </html>
