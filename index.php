48c88459731196595b5ba25b4f357986b1648fc8
Davi Toledo <davi_toledo0@hotmail.com>
2019-12-02 17:48:50 -0200

    phpinfo

--- index.php
+++ index.php
@@ -1,14 +1,3 @@
-<html>
- <head>
-  <title>526 - Curso Exin DevOps Master</title>
- </head>
- <body>
- <?php 
-echo "<p>Olá Mundo! Somos Devopeiros ! </p>";
-echo "<p> Exemplo de deploy contínuo com ferramentas SaaS </p>";
- ?>
-
-<img src="https://www.vanharen.net//media/catalog/product/cache/97/image/9df78eab33525d08d6e5fb8d27136e95/e/x/exin_devops_master.jpg" />
-
- </body>
-</html>
+<? php
+phpinfo ();
+?> 
